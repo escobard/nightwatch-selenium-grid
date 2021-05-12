5767dddfbbc744ff97eafdb188f42dc0be3cfd78
danielescobar-mdous <danielescobar@mydigitaloffice.ca>
2021-05-11 19:35:43 -0600

    feat: add base nightwatch configurations with chromedriver

---
+++ constants/config.js
@@ -0,0 +1,22 @@
+const chrome = require("chromedriver");
+
+const clientHost = process.env.UI_HOST || 'localhost',
+  clientPort = process.env.UI_PORT || '3000',
+  clientUrl = `http://${clientHost}:${clientPort}`,
+  webDriver = process.env.DOCKER ? null : {
+    start_process: true,
+    server_path: chrome.path,
+    port: 9515
+  },
+  seleniumHub = process.env.SELENIUM_HUB || null;
+
+
+const config = {
+  url: clientUrl,
+  seleniumHost: process.env.SELENIUM_HOST,
+  seleniumPort: process.env.SELENIUM_PORT,
+  webDriver,
+  seleniumHub
+};
+
+module.exports = config;
